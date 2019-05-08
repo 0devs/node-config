2a2eecc664c0a0ee1ba8ee77f60f5fc1bfdce036
alekzonder <alekzonder@gmail.com>
2019-05-08 13:08:49 +0700

    chg: Config Error message text

--- src/Error.ts
+++ src/Error.ts
@@ -3,19 +3,21 @@ import Zerror from "@0devs/error";
 /**
  * config error class with codes
  */
-class ConfigError extends Zerror {}
+class ConfigError extends Zerror {
+  public static CODES = {
+    INVALID_LOGGER: "invalid logger",
+    INVALID_VALIDATION_FUNCTION: "invalid validation function",
+    INVALID_ARGS: "invalid method arguments",
+    INVALID_DATA: "invalid data",
+    INVALID_PLUGIN: "invalid plugin",
+    INVALID_PLUGIN_TYPE: "invalid plugin typr",
+    INVALID_PLUGIN_READ: "invalid plugin read function, should return promise",
+    IMMUTABLE: "config is immutable, see .setImmutable method",
+    UNKNOWN_SOURCE_TYPE: "inknown source type, no plugin for source",
+    FAILED_TO_READ_SOURCE: "failed to read source, plugin error",
+  };
+}
 
-ConfigError.setCodes({
-  INVALID_LOGGER: "INVALID_LOGGER",
-  INVALID_VALIDATION_FUNCTION: "INVALID_VALIDATION_FUNCTION",
-  INVALID_ARGS: "INVALID_ARGS",
-  INVALID_DATA: "INVALID_DATA",
-  INVALID_PLUGIN: "INVALID_PLUGIN",
-  INVALID_PLUGIN_TYPE: "INVALID_PLUGIN_TYPE",
-  INVALID_PLUGIN_READ: "INVALID_PLUGIN_READ",
-  IMMUTABLE: "IMMUTABLE",
-  UNKNOWN_SOURCE_TYPE: "UNKNOWN_SOURCE_TYPE",
-  FAILED_TO_READ_SOURCE: "FAILED_TO_READ_SOURCE",
-});
+ConfigError.setCodes(ConfigError.CODES);
 
 export default ConfigError;
